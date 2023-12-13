5670237e06c58b5c21f6036a20a9f36009cc123f
Ivan.Pokhilchenko <ivanpohilchenko1999@gmail.com>
2023-12-13 19:46:43 +0100

    new_file6

---
+++ Homework_SQL/SQL_hw-6.sql
@@ -0,0 +1,16 @@
+-- Создать таблицы реакций и комментариев
+
+-- Добавить несколько записей в каждую таблицу выше
+
+CREATE TABLE comments (
+    comments_id int,
+    created_at timestamp,
+    author_id int,
+    video_id int,
+    content varchar(128)
+);
+
+INSERT INTO comments (comments_id, created_at, author_id, video_id, content)
+VALUES
+    (1, CURRENT_TIMESTAMP, 1, 1, 'Good video!'),
+    (2, CURRENT_TIMESTAMP, 2, 2, 'Good job, user!');
